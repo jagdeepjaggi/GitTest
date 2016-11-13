717c5e7e73d52017b19efd9c246866b54de54705
Jagdeep Singh Jaggi <jagdeep1691jaggi@gmail.com>
2016-11-13 10:31:13 -0500

    fifth commit to test results

---
+++ GitJSPTesting/src/resources/GitJSPTestingTest.java
@@ -0,0 +1,24 @@
+package resources;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GitJSPTestingTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final String test() {
+		return "This is test result"; // TODO
+	}
+
+}
